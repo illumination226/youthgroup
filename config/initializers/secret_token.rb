b9745ac00de276e888a309c9b04a5162c69160ca
illumination226 <illuminationdevelopment@gmail.com>
2011-07-23 22:15:43 +0000

    create rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youthgroup::Application.config.secret_token = '4b31ec1eaa618fc7188377abb29a22f74bd36c3992ae1c43e105cf17d6cc3ab9b448dd3e94fad89efd271f0b48d2aa06f67007b8ab3a17ca0c1900bd15f12581'
