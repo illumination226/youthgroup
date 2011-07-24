e2bceafd18954f4093290385ec1f4197681b8ada
illumination226 <illuminationdevelopment@gmail.com>
2011-07-24 13:33:02 -0700

    add style

--- config/routes.rb
+++ config/routes.rb
@@ -49,10 +49,6 @@ Youthgroup::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
   
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
@@ -62,5 +58,5 @@ Youthgroup::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
